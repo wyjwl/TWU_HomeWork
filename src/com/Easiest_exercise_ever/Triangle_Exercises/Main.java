2cde4d64d4261cfe659d95135d8576e77ba72043
wyjwl <396651773@qq.com>
2016-06-03 19:15:28 +0800

    Easiest exercise ever

---
+++ src/com/Easiest_exercise_ever/Triangle_Exercises/Main.java
@@ -0,0 +1,10 @@
+package com.Easiest_exercise_ever.Triangle_Exercises;
+
+/**
+ * Created by WYJ on 2016/6/3.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("*");
+    }
+}
